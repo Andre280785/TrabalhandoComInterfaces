844f9e4eea200f34592dcb0fbc6fd9e252d86223
André Tavares Machado <andre266@outlook.com.br>
2020-04-05 22:42:19 -0300

    Aluguel de Carros Sem utilizar interfaces.

---
+++ AluguelDeCarro/Entities/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿
+namespace AluguelDeCarro.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+
+    }
+}
---
+++ AluguelDeCarro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using AluguelDeCarro.Entities;
+
+namespace AluguelDeCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            String model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+    }
+}
